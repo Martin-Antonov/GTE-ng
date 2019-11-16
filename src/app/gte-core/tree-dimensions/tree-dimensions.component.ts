7529c7b59cabef168d887d18a94bef319aa988f6
Martin Antonov <antonov.martin@gmail.com>
2019-11-16 10:14:46 +0200

    pre-undo commit

---
+++ src/app/gte-core/tree-dimensions/tree-dimensions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tree-dimensions',
+  templateUrl: './tree-dimensions.component.html',
+  styleUrls: ['./tree-dimensions.component.scss']
+})
+export class TreeDimensionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
